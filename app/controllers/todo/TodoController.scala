810c7ecacaec8a4035f023088508b34460ff03ab
Jun-Honda-0326 <j.honda.0326@gmail.com>
2020-11-04 16:47:32 +0900

    todoを一覧表示

--- app/controllers/todo/TodoController.scala
+++ app/controllers/todo/TodoController.scala
@@ -10,6 +10,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import lib.model.Todo
 import lib.persistence.default.TodoRepository
 import model.ViewValueTodo
+import akka.http.scaladsl.model.headers.LinkParams.title
 //import play.api.i18n.I18nSupport
 
 @Singleton
@@ -21,16 +22,18 @@ class TodoController @Inject()(
     for {
       todos <- TodoRepository.all()
     } yield {
-      val vv = ViewValueTodo(
-        "一覧表示",
-        Seq("main.css"),
-        Seq("main.js"),
-        todos.map(_.v.id.get),
-        todos.map(_.v.categoryId),
-        todos.map(_.v.title),
-        todos.map(_.v.content),
-        todos.map(_.v.state)
-        ) 
+        val vv = ViewValueTodo(
+          head = "一覧表示",
+          cssSrc = Seq("main.css"),
+          jsSrc  = Seq("main.js"),
+          todo = todos.map(_.v)
+
+      //  id =todos.map(_.v.id.get),
+      //  categoryId = todos.map(_.v.categoryId),
+      //  title = todos.map(_.v.title),
+      //  content = todos.map(_.v.content),
+      //  state = todos.map(_.v.state),
+    )
       Ok(views.html.todo.list(vv))
     }
   }
--- app/model/Todo.scala
+++ app/model/Todo.scala
@@ -1,14 +1,12 @@
 package model
 
+import lib.model.Todo
 import lib.model.Todo.TodoStatus
 
 case class ViewValueTodo(
   head:       String,
   cssSrc:     Seq[String],
   jsSrc:      Seq[String],
-  id:         Seq[Long],
-  categoryId: Seq[Long],
-  title:      Seq[String],
-  content:    Seq[String],
-  state:      Seq[TodoStatus]
+  todo:       Seq[Todo]
 )extends ViewValueCommon
+
