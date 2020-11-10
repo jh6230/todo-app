8f21d9ffca142aaacac5c3a094b12a5ff99cb137
Jun-Honda-0326 <j.honda.0326@gmail.com>
2020-11-10 10:53:06 +0900

    ViewValueCategoryForm定義

--- app/model/Category.scala
+++ app/model/Category.scala
@@ -2,6 +2,9 @@ package model
 
 import lib.model.Category
 import lib.model.Category.CategoryColor
+import controllers.category.CategoryForm
+import play.api.data.Form
+import play.api.data.Form._
 
 case class ViewValueCategory(
   head:     String,
@@ -10,3 +13,9 @@ case class ViewValueCategory(
   category: Seq[Category]
 )extends ViewValueCommon
 
+case class ViewValueCategoryForm(
+  head:      String,
+  cssSrc:    Seq[String],
+  jsSrc:     Seq[String],
+  category:  Form[CategoryForm]
+)extends ViewValueCommon
