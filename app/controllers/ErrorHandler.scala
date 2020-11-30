b7f5b20f3fa4417d88eec9dbdd8bd981a42115ee
Jun-Honda-0326 <j.honda.0326@gmail.com>
2020-11-30 18:57:39 +0900

    カテゴリーリストのテーブルのサイズを大きくする

--- app/controllers/ErrorHandler.scala
+++ app/controllers/ErrorHandler.scala
@@ -1,36 +1,25 @@
-import javax.inject._
-import play.api.inject.Binding
-import play.api._
-import play.api.mvc._
-import play.api.http
-import play.mvc.Http
-import play.api.mvc.Results._
 import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.mvc.Result
 import scala.concurrent._
-import play.api.routing.Router
-import views.html.defaultpages.todo
+import javax.inject.Singleton
+import play.api.http.Writeable._
 
 @Singleton
-class ErrorHandler extends HttpErrorHandler{
+class ErrorHandler extends HttpErrorHandler {
 
-  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {  
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
     Future.successful(
-      Status(statusCode)("A client error occurred" + message)
-      )
+      Status(statusCode)("A client error occurred: " + message)
+    )
   }
 
-  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
-
+  def onServerError(request: RequestHeader, exception: Throwable) = {
     Future.successful(
-      InternalServerError("A server error occurred" + exception.getMessage)
-//      InternalServerError(Redirect(controllers.routes.TodoController.list()))
-
-
-
-
-      )
+      InternalServerError("A server error occurred: " + exception.getMessage)
+//      InternalServerError(Redirect(controllers.todo.routes.TodoController.list()))
+    )
   }
-
-
 }
 
